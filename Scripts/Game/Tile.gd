cdde6798ea60df6e34654dc46efb49e619912b6e
Cyp P <cypriane81@gmail.com>
2024-05-04 15:53:40 +0100

    [+Added] Pollution???

--- Scripts/Game/Tile.gd
+++ Scripts/Game/Tile.gd
@@ -1,3 +1,45 @@
 extends StaticBody3D
 
+class_name Tile
+
 @export var selectOffset: Vector3 = Vector3(0, 0.75, 0)
+
+var health: float = 100.0
+
+var maxHealth: float = 100.0
+
+var directPollution: float = 0.0 # Pollution from a direct pollution source
+var indirectPollution: float = 0.0 # Pollution from nearby polluted tiles
+
+var gatheredPollution: float = 0.0 # Pollution gathered this tick
+
+var tilesInArea: Array[Tile] = []
+
+@export var healthyColor: Color = Color(0.0, 1.0, 0.0, 1.0)
+@export var unhealthyColor: Color = Color(1.0, 0.0, 0.0, 1.0)
+
+func UpdateArea(body: Node3D, enter: bool):
+    if body is Tile:
+        if enter:
+            tilesInArea.append(body)
+        else:
+            tilesInArea.erase(body)
+
+func SendPollution():
+    for tile in tilesInArea:
+        tile.gatheredPollution += ((directPollution / 1.5) + (indirectPollution / 4.0)) * (2 - position.distance_to(tile.position))
+        
+
+func ApplyIndirect():
+    indirectPollution += gatheredPollution * 0.05
+
+    indirectPollution *= 0.9
+    if indirectPollution + directPollution > 100.0:
+        indirectPollution = 100.0 - directPollution
+
+    $DebugPollution.text = str(snapped(indirectPollution + directPollution, 0.01))
+    gatheredPollution = 0.0
+
+    health = clamp(maxHealth - indirectPollution - directPollution, 0.0, maxHealth)
+
+    $Mesh.mesh.material.albedo_color = healthyColor.lerp(unhealthyColor, 1.0 - (health / maxHealth))
--- Scripts/Game/World.gd
+++ Scripts/Game/World.gd
@@ -6,28 +6,44 @@ static var ins: World
 @export var tileIns: PackedScene
 var select: Node3D
 
+var tilesInWorld: Array[Tile] = []
+
+var pollutionTick: float = 0.0
+
 func _init():
 	ins = self
 
 func _ready():
 	select = $Select
 	select.visible = false
-	for i in range(5):
-		for j in range(5):
+	for i in range(15):
+		for j in range(15):
 			var tile = tileIns.instantiate()
 			tile.position = Vector3(i, 0, j)
 			add_child(tile)
-	Cam.ins.Focus(Vector3(2, 0, 2), false)
+			tilesInWorld.append(tile)
+	Cam.ins.Focus(Vector3(7, 0, 7), false)
 
-func _process(_dt):
+func _process(dt):
 	var tile = SelectRay()
 	if tile != null:
 		if Input.is_action_just_pressed("Select"):
 			Cam.ins.Focus(tile.position)
+			tile.directPollution = 50.0
 		select.position = tile.position + tile.selectOffset
 		select.visible = true
 	else:
 		select.visible = false
+	pollutionTick -= dt
+	if pollutionTick < 0.0:
+		UpdateTiles()
+		pollutionTick += 0.5
+
+func UpdateTiles():
+	for tile in tilesInWorld:
+		tile.SendPollution()
+	for tile in tilesInWorld:
+		tile.ApplyIndirect()
 
 func SelectRay() -> Node3D:
 	var camOrigin = Cam.ins.project_ray_origin(get_viewport().get_mouse_position())
